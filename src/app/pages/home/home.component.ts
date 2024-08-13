e924ac19c16c201b5a5178a5b7391c01337e5ee3
Antonio Saborido <antonio.saborido01@gmail.com>
2024-08-13 19:17:28 +0200

    save users score into my db

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,23 +1,51 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
+import {API_URL} from "../../../config";
+import {NgForOf} from "@angular/common";
+
+interface UserScore {
+  username: string;
+  score: number;
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    NgForOf
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-  constructor(private router: Router) {}
+export class HomeComponent implements OnInit {
+  topUsers: UserScore[] = [];
+
+  constructor(private router: Router, private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchTopUsers();
+  }
 
   startGame() {
     this.router.navigate(['/play']);
   }
+
   goToOptions() {
     this.router.navigate(['/play']);
+  }
+
+  fetchTopUsers() {
+    this.http.get<UserScore[]>(`${API_URL}/api/plataformas/top10`).subscribe(
+      (data: UserScore[]) => {
+        this.topUsers = data;
+      },
+      (error: any) => {
+        console.error('Error fetching top users:', error);
+      }
+    );
   }
 }
